784d9dcc75cdd50b1033e86ff66e3a1488b34f73
sheikhtoocool <harissheikh255@gmail.com>
2023-03-26 21:07:05 +0500

    Second Commit Commented

--- App.js
+++ App.js
@@ -1,19 +1,24 @@
-import { createAppContainer } from "react-navigation";
-import { createStackNavigator } from "react-navigation-stack";
+//This is our main component or parent component i.e the Navigator. As we are using multiple screens here
+//so we are using this navigation in order to navigate in these screens.
+
+import { createAppContainer } from "react-navigation";//this will be parent container in which all screens will be placed
+import { createStackNavigator } from "react-navigation-stack";//this is a specific type of navigation that we are using
+//there are other than stack too i.e drawer and tab navigation
 import HomeScreen from "./src/screens/HomeScreen";
 import SecondScreen from "./src/screens/ComponentScreen";
 
 const navigator = createStackNavigator(
   {
+    //create two screens variables
     Home: HomeScreen,
     Second: SecondScreen,
   },
   {
-    initialRouteName: "Home",
+    initialRouteName: "Home",//initial screen is Home i.e app will start from home
     defaultNavigationOptions: {
       title: "GalleryApp",
     },
   }
 );
 
-export default createAppContainer(navigator);
+export default createAppContainer(navigator);//export navigator component
--- src/screens/ComponentScreen.js
+++ src/screens/ComponentScreen.js
@@ -1,57 +1,52 @@
 import React from 'react';
-import { StyleSheet, Text, View, FlatList, Image,TouchableOpacity, Linking, Share } from 'react-native';
-
- async function openWhatsapp(
+import { Dimensions } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Image, TouchableOpacity, Share } from 'react-native';
+const width = Dimensions.get('window').width;//full width
+const height = Dimensions.get('window').height; //full 
+//we are using async function here as we want to check for the outcome of this function which is either 
+//image sent or not sent
+async function openWhatsapp(
   phone,
   photoUrl,
 ) {
+  
   try {
-      await Share.share({
-        title: 'Image',
-        message: 'Shared By Haris Sheikh\n'+photoUrl,
-        url: photoUrl,
-        subject: '',
-        social: 'whatsapp'
-      });
-    
+    await Share.share({//Share Function is accessed using Share Component
+      title: 'Image',
+      message: 'Shared By Haris Sheikh\n' + photoUrl,
+      //url: photoUrl,
+      subject: '',
+      social: 'whatsapp'
+    });
+
   } catch (err) {
-    console.log({ title: 'installApplication' });
+    console.log('Please install WhatsApp.');
   }
 }
 
 const SecondScreen = (props) => {
-  const {photo, id} = props.navigation.state.params.params.image;
-  const val=props.navigation.state.params.params.details;
-  const removeImage =props.navigation.state.params.params.removeImage;
+  //using props, we can de struct its objects and access the params passed in previous screen
+  const { photo, id } = props.navigation.state.params.params.image;
+  const val = props.navigation.state.params.params.details;
+  const removeImage = props.navigation.state.params.params.removeImage;//
 
-  return  <View style={{ justifyContent: "center", alignItems: "center", flex: 1, }}>
-          <Image source={{ uri: photo }} style={{ width: 300, height: 300 }} />
-          <Text>{val}</Text>
-   <View style={{flexDirection: 'row'}}>
-    <TouchableOpacity style={{padding: 20, backgroundColor:'grey'}} 
-    onPress={()=> openWhatsapp('+923414742424', photo) }><Text style={{color: 'white'}}>{'Share on Wtsap'}</Text></TouchableOpacity>
-    <TouchableOpacity style={{padding: 20, backgroundColor:'grey'}} onPress={()=> {removeImage(id)
-    props.navigation.navigate('Home')} }><Text style={{color: 'white'}}>{'Delete Image'}</Text></TouchableOpacity>
+  return (<View style={{ alignItems: "center", flex: 1, padding: 20 }}>
+    <Image source={{ uri: photo }} style={{ width: width, height: height / 2 }} />
+    <Text style={{ fontSize: 26, fontWeight: 'bold' }}>Details</Text>
+    <Text style={{ fontSize: 20 }}>{val}</Text>
+
+    <View style={{ flexDirection: 'row' }}>
+      <TouchableOpacity style={{ padding: 20, backgroundColor: 'green' }}
+      //Here we will call the share function
+        onPress={() => openWhatsapp('+923414742424', photo)}><Text style={{ color: 'white' }}>{'Share on Wtsap'}</Text></TouchableOpacity>
+      <TouchableOpacity style={{ padding: 20, backgroundColor: 'red' }} onPress={() => {
+        //Here we used removeImage that is accessed using params of props,
+        removeImage(id)
+        props.navigation.navigate('Home')
+      }}><Text style={{ color: 'white' }}>{'Delete Image'}</Text></TouchableOpacity>
     </View>
-    </View>
-  
+  </View>);
+
 };
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      marginTop: 100,
-    },
-    item: {
-      backgroundColor: '#f9c2ff',
-      padding: 20,
-      marginVertical: 8,
-      marginHorizontal: 16,
-      flex: 1,
-      borderRadius: 5,
-    },
-    title: {
-      fontSize: 32,
-    },
-});
 
-export default(SecondScreen) 
+export default (SecondScreen) 
